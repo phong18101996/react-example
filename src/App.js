192b3d816e65f4f0561a1aac05834cf1695bcd12
phong18101996 <phong18101996@gmail.com>
2021-08-09 10:55:34 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
+import TodoList from './components/todoList';
+import Textfield from '@atlaskit/textfield';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  return  (
+    <>
+      <h3>Danh sach can lam</h3>
+      <Textfield name="add-todo"></Textfield>
+    </>
   );
 }
 
---
+++ src/components/todoList.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export default function todoList(){
+    return (
+        <h3>danh sach can lam</h3>
+    )
+}
